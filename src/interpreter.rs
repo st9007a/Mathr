209bb28062840ee991b1779e945a2525d7a730c6
UnoWu <st9007a@gmail.com>
2022-10-13 20:28:27 +0800

    Rename global to symtab

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,19 +3,19 @@ use std::collections::HashMap;
 use crate::ast::StatementListNode;
 
 pub struct Interpreter {
-    global: HashMap<String, i32>,
+    symtab: HashMap<String, i32>,
     nodes: Vec<Box<StatementListNode>>,
 }
 
 impl Interpreter {
     pub fn new() -> Self {
-        let mut global: HashMap<String, i32> = HashMap::new();
+        let mut symtab: HashMap<String, i32> = HashMap::new();
 
-        global.insert("e".to_string(), 2);
-        global.insert("pi".to_string(), 3);
+        symtab.insert("e".to_string(), 2);
+        symtab.insert("pi".to_string(), 3);
 
         Self {
-            global,
+            symtab,
             nodes: vec![],
         }
     }
