2d8107143599be1a82333514afe37b257cbfc322
UnoWu <st9007a@gmail.com>
2022-10-15 18:28:28 +0800

    Test symbol table

--- src/ast/number.rs
+++ src/ast/number.rs
@@ -26,7 +26,42 @@ impl ASTExpression for NumberNode {
 }
 
 impl ASTSemanticAnalysis for NumberNode {
-    fn check_semantic(&self, symtab: &mut SymbolTable) -> Result<(), InterpreterError> {
+    fn check_semantic(&self, _symtab: &mut SymbolTable) -> Result<(), InterpreterError> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::symbol_table::SymbolTable;
+
+    use super::ASTExpression;
+    use super::ASTSemanticAnalysis;
+    use super::NumberNode;
+
+    #[test]
+    fn test_eval() {
+        let mut symtab = SymbolTable::new();
+        let node = NumberNode::new(23.);
+        let result = node.eval(&mut symtab);
+
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 23.);
+    }
+
+    #[test]
+    fn test_pure() {
+        let node = NumberNode::new(2.31);
+        assert!(node.pure());
+    }
+
+    #[test]
+    fn test_check_semantic() {
+        let mut symtab = SymbolTable::new();
+        let node = NumberNode::new(0.1234);
+
+        let result = node.check_semantic(&mut symtab);
+
+        assert!(result.is_ok());
     }
 }
